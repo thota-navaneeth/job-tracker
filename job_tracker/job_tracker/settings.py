267ad39c13a653704847e142da985993bf7b5d2e
Thota Navaneeth <thotanavaneeth121@gmail.com>
2026-05-01 21:09:01 +0530

    Fix app import path

--- job_tracker/job_tracker/settings.py
+++ job_tracker/job_tracker/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracker',
+    'job_tracker.tracker',
 ]
 
 MIDDLEWARE = [
