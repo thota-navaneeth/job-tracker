73b14c362dba9e93d3424b1fe301148356e2b5e0
Thota Navaneeth <thotanavaneeth121@gmail.com>
2026-05-01 20:52:27 +0530

    Add database config

--- job_tracker/job_tracker/settings.py
+++ job_tracker/job_tracker/settings.py
@@ -13,6 +13,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
+PORT = os.environ.get("PORT", 8000)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
