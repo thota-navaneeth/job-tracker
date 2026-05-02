71fdb861cda9c7213f3a393fdad76dd2f5ef2c6d
Thota Navaneeth <thotanavaneeth121@gmail.com>
2026-05-02 22:23:18 +0530

    Fix wsgi path after restructure

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job_tracker.tracker',
+    'tracker',
 ]
 
 MIDDLEWARE = [
